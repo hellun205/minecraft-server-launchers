b3a2c9f0c86ce8845faec0e4b98f48b4a4d05566
hellun205 <hellun515@gmail.com>
2021-06-02 23:33:19 +0900

    server class

---
+++ Main.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin.Controls;
+using MaterialSkin;
+
+namespace minecraft_server_launchers
+{
+  public partial class Main : MaterialForm
+  {
+    private static MaterialSkinManager msm = MaterialSkinManager.Instance;
+    public Main()
+    {
+      InitializeComponent();
+
+      msm.AddFormToManage(this);
+      msm.Theme = MaterialSkinManager.Themes.DARK;
+      ChangeColor("blue");
+    }
+
+    private void ChangeColor(string colorS = "blue-gray")
+    {
+      ColorScheme cs;
+      switch (colorS)
+      {
+        case "blue":
+          cs = new ColorScheme(Primary.Blue800, Primary.Blue900, Primary.Blue500, Accent.LightBlue200, TextShade.WHITE);
+          break;
+        case "green":
+          cs = new ColorScheme(Primary.Green800, Primary.Green900, Primary.Green500, Accent.LightGreen200, TextShade.WHITE);
+          break;
+        case "red":
+          cs = new ColorScheme(Primary.Red800, Primary.Red900, Primary.Red500, Accent.Orange200, TextShade.WHITE);
+          break;
+        case "yellow":
+          cs = new ColorScheme(Primary.Yellow800, Primary.Yellow900, Primary.Yellow500, Accent.Orange200, TextShade.BLACK);
+          break;
+        case "blue-gray":
+        default:
+          cs = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+          break;
+      }
+      msm.ColorScheme = cs;
+    }
+
+  }
+}
---
+++ Server.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace minecraft_server_launchers
+{
+  class Server
+  {
+    private Process process = new Process();
+    public int MaxRam { get; set; }
+    public int MinRam { get; set; }
+    public string BukkitPath { get; set; }
+    public string Data
+    {
+      get => data;
+      private set
+      {
+        data = value;
+        Output.AppendText($"\n{data}");
+      }
+    }
+    private string data;
+    public bool IsOnline = false;
+
+    public Action OnStarted = () => { };
+    public Action OnExited = () => { };
+
+    public RichTextBox Output { get; set; }
+
+    public void Start()
+    {
+      try
+      {
+        ExecuteServ();
+      }
+      catch
+      {
+        process.Dispose();
+        process = new Process();
+      }
+    }
+
+    private void ExecuteServ()
+    {
+      process.StartInfo.FileName = "java.exe";
+      process.StartInfo.Arguments = $"-Djline.terminal=jline.UnsupportedTerminal -Xmx{MaxRam}G -Xms{MinRam}G - jar \"{BukkitPath}\" nogui";
+      process.StartInfo.CreateNoWindow = true;
+      process.StartInfo.UseShellExecute = false;
+      process.StartInfo.RedirectStandardOutput = true;
+      process.StartInfo.RedirectStandardInput = true;
+      process.StartInfo.RedirectStandardError = true;
+
+      process.OutputDataReceived += new DataReceivedEventHandler(OnOutput);
+      process.ErrorDataReceived += new DataReceivedEventHandler(OnOutput);
+      process.Exited += new EventHandler(Exited);
+
+      process.Start();
+      process.EnableRaisingEvents = true;
+      process.BeginErrorReadLine();
+      process.BeginOutputReadLine();
+      OnStarted();
+    }
+
+    public void Stop()
+    {
+      process.Kill();
+    }
+
+    private void OnOutput(object sender, DataReceivedEventArgs e)
+    {
+      Data = e.Data;
+    }
+
+    public void Input(string inputCommand)
+    {
+      if (IsOnline) process.StandardInput.WriteLine(inputCommand);
+    }
+
+    private void Exited(object sender, EventArgs e)
+    {
+      process.Dispose();
+      process = new Process();
+      OnExited();
+    }
+    
+  }
+}
