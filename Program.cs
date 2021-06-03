7d0cb50782cdc96696ba4bbb87d5ea5386f690ed
hellun205 <hellun515@gmail.com>
2021-06-03 18:05:39 +0900

    a

--- Program.cs
+++ Program.cs
@@ -1,23 +1,20 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace minecraft_server_launchers
 {
-    static class Program
+  static class Program
+  {
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
-        }
+      Application.SetHighDpiMode(HighDpiMode.SystemAware);
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new Main());
     }
+  }
 }
